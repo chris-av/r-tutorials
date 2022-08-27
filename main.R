cf6011582716f6470409332b26e58d093b24c178
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-08-27 13:27:49 -0700

    add some notes to main

--- main.R
+++ main.R
@@ -1,1 +1,8 @@
 # see each folder for individualized tutorials on how to do certain tasks
+
+# files prepended with 000_ mean that they are useful information to the topic at hand
+# other files are prepended with 001_, 002_,
+#    for these, I tried organizing the files from easiest/basic to hardest/complex
+# BEWARE: be mindful of what libraries you have on your computer
+#    for instance, library(stargazer) implicitly assumes that at some point,
+#    you downloaded the stargazer package using install.packages("stargazer")
